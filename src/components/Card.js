482991ed5be548e183c20015a47f0f7060fa0b7a
Shivanshu Misra <shivanshu.misra-acn@hpe.com>
2021-07-13 17:46:10 +0530

    Css changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card =({ name, email, id}) =>{
     //const { name, email, id} = props;
     return(
         <div className='tc ma2 bg-light-green dib br3 pa2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
+            <img  src={`https://robohash.org/${id}?size=200x200`} alt='robots'/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
