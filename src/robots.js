78c9066e03cbec108526bf025db3542dea52cbce
Shivanshu Misra <shivanshu.misra-acn@hpe.com>
2021-07-13 17:27:20 +0530

    My new react projetc

---
+++ src/robots.js
@@ -0,0 +1,43 @@
+export const robots = [
+    {
+        id:1,
+        name:'Shivanshu Misra',
+        username: 'Shiv',
+        email: 'shivanshu.misra9@gmail.com'
+    },
+    {
+        id:2,
+        name:'Anant Misra',
+        username: 'Anna',
+        email: 'annant.misra@gmail.com'
+    },
+    {
+        id:3,
+        name:'Akriti Misra',
+        username: 'Akriti',
+        email: 'akriti.misra9@gmail.com'
+    },
+    {
+        id:4,
+        name:'Himanshu Misra',
+        username: 'Himan',
+        email: 'himanshu.misra9@gmail.com'
+    },
+    {
+        id:5,
+        name:'Alankriti Misra',
+        username: 'Alank',
+        email: 'alankriti.misra9@gmail.com'
+    },
+    {
+        id:6,
+        name:'Umesh Misra',
+        username: 'Umesh',
+        email: 'umesh.misra9@gmail.com'
+    },{
+        id:7,
+        name:'Kalpana Misra',
+        username: 'Kalp',
+        email: 'kalapana.misra9@gmail.com'
+    }
+]
